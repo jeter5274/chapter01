683d5e220fa1c63f55f3ff639c1305d6458a9ca8
jeter5274 <jeter5274@gmail.com>
2020-11-13 15:33:05 +0900

    9, 10예제 추가 및 이미지 이름 변경

---
+++ src/com/javaex/ex03/Ex10.java
@@ -0,0 +1,47 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		
+		int month;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("월을 입력하세요");
+		month = sc.nextInt();
+		
+		switch(month) {
+			case 2 : 
+				System.out.println("28일 입니다.");
+				break;
+			/*
+			case 4 : 
+				System.out.println("30일 입니다.");
+				break;
+			case 6 : 
+				System.out.println("30일 입니다.");
+				break;
+			case 9 : 
+				System.out.println("30일 입니다.");
+				break;
+			case 11 : 
+				System.out.println("30일 입니다.");
+				break;
+			*/
+			case 4 : 
+			case 6 : 
+			case 9 : 
+			case 11 : 
+				System.out.println("30일 입니다.");
+				break;
+			default : 
+				System.out.println("31일 입니다.");
+				break;
+		}
+		
+		sc.close();
+	}
+
+}
